2ec1e7f08a2e59cfc884b79995cf8b2d86934a04
Osama Aly <70722589+osama-aly-oa@users.noreply.github.com>
2025-09-18 17:04:47 +0300

    Add files via upload

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=DVLD;Trusted_Connection=True;";
+
+
+    }
+}
